06b0eb77037717bbf209d190be6fdcf28087d382
Chris Allan <cjamesallan@gmail.com>
2017-05-03 20:00:15 -0700

    Assembler class

--- asm.pro
+++ asm.pro
@@ -3,4 +3,9 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    assembler.cpp
+
+HEADERS += \
+    common_types.h \
+    assembler.h
--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
+/**
+ * Assembler for SAP 1.5
+ */
+
+#include <iomanip>
 #include <iostream>
+#include <string>
+
+#include <boost/format.hpp>
+
+#include "assembler.h"
 
 using namespace std;
 
+void print_use();
+
 int main(int argc, char *argv[])
 {
-	cout << "Hello World!" << endl;
+	if (argc < 2) {
+		print_use();
+		return -1;
+	}
+	
+	string arg { argv[1] };
+	
+	Assembler a { arg };
+	
+	if (a.good()) {
+		cout << hex << showbase;
+		int i = 0;
+		for (byte b : a.mcode()) {
+			cout << boost::format("0x%02x") % i << " " << boost::format("0x%02x") % (int) b << endl;
+			i++;
+		}
+	}
+	
+	
 	return 0;
 }
+
+void print_use()
+{
+	
+}
+
